0ce7b7e42b3c5a87974642fe492a122bb9f8a298
DESKTOP-KPJEHFP\Jimi Taufik <kalitrack354@gmail.com>
2021-03-13 15:18:55 +0800

    event bangkit

--- Memulai Pemrograman dengan Kotlin 2019.1.4/src/main/kotlin/Numbers/Int val.kt
+++ Memulai Pemrograman dengan Kotlin 2019.1.4/src/main/kotlin/Numbers/Int val.kt
@@ -4,8 +4,18 @@ import kotlin.math.min
 
 fun main(){
     val maxInt = Int.MAX_VALUE
+
     val minInt = Int.MIN_VALUE
 
-    print(maxInt)
-    print(minInt)
+    println(maxInt)
+    println(minInt)
+
+    val byteNumber : Byte = 10
+    val intNumber : Int = byteNumber.toInt()
+
+    println("byteNumber is $byteNumber ")
+    print("int Number is $intNumber ")
+
+
+
 }
